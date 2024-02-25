0de16c713b3c77b31c4d1a714c5de0fe7d2e555a
asg14826 <148794186+asg14826@users.noreply.github.com>
2024-02-25 20:19:57 +0200

    n

---
+++ app.js
@@ -0,0 +1,12 @@
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+    
+}
+let a= 1
+
+while (a<10) {
+   console.log(('naanch is the tzadik')); 
+   a++
+}
+console.log('a');
+console.log('b');
